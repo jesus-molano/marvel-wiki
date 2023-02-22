03894dff356828a5b8c25369443a2f528c9f89dd
jesus-molano <jessumolano@gmail.com>
2023-02-22 02:45:01 +0000

    Added simple pagination

--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -1,12 +1,34 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CharacterItem } from '@components/Characters/CharacterItem'
 import { CharactersContext } from '@context/index'
 import { MainLayout } from '@layouts/MainLayout'
+import { getCharacters } from '@apis/getCharacters'
 
 export const CharactersPage = () => {
-  const { charactersData } = useContext(CharactersContext)
-  if (!charactersData) return null
-  const { characters, hasError, isLoading } = charactersData
+  const total = 40
+  const limit = 10
+  const [offset, setOffset] = useState(0)
+  const { charactersData, setCharactersData } = useContext(CharactersContext)
+
+  const { characters, isLoading, hasError } = getCharacters(limit, offset)
+  const handleNextPage = () => {
+    
+    if(offset >= total) return
+    setOffset(offset + 10)
+  }
+  const handlePrevPage = () => {
+    if(offset <= 0) return
+    setOffset(offset - 10)
+  }
+
+  useEffect(() => {
+    setCharactersData({
+      characters,
+      isLoading,
+      hasError
+    })
+  }, [offset])
+
 
   return (
     <MainLayout title='Characters'>
@@ -18,6 +40,13 @@ export const CharactersPage = () => {
             <CharacterItem key={character.id} character={character} />
           ))}
       </div>
+      <button onClick={handlePrevPage}>
+      {'<'}
+      </button>
+      <button onClick={handleNextPage}>
+        {'>'}
+      </button>
+
     </MainLayout>
   )
 }
