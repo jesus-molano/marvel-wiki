ac2522275c6cfe081504398737168b061448a58c
jesus-molano <jessumolano@gmail.com>
2023-02-22 18:00:14 +0000

    Added styles to character detail

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,14 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { CharactersPage, ComicsPage, NotFoundPage, SeriesPage } from '@pages/index'
-import {CharacterDetail} from '@pages/CharacterDetail'
+import {CharacterDetailPage} from '@pages/CharacterDetailPage'
 const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Navigate to='/characters' />} />
         <Route path='/characters' element={<CharactersPage />} />
-        <Route path='/characters/:characterId' element={<CharacterDetail />} />
+        <Route path='/characters/:characterId' element={<CharacterDetailPage />} />
         <Route path='/comics' element={<ComicsPage />} />
         <Route path='/series' element={<SeriesPage />} />
         <Route path='*' element={<NotFoundPage />} />
