daa80b68fa135af119d5efc4fdf739245110421c
jesus-molano <jessumolano@gmail.com>
2023-02-22 16:49:58 +0000

    Added routes for characterDetail

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { CharactersPage, ComicsPage, NotFoundPage, SeriesPage } from '@pages/index'
+import {CharacterDetail} from '@pages/CharacterDetail'
 const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Navigate to='/characters' />} />
         <Route path='/characters' element={<CharactersPage />} />
+        <Route path='/characters/:characterId' element={<CharacterDetail />} />
         <Route path='/comics' element={<ComicsPage />} />
         <Route path='/series' element={<SeriesPage />} />
         <Route path='*' element={<NotFoundPage />} />
