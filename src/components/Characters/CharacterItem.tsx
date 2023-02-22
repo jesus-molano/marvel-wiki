81af4c2feb68fe40965e50165a2a9e1c2f5a6fb3
jesus-molano <jessumolano@gmail.com>
2023-02-22 13:33:42 +0000

    Fixed characterItem link

--- src/components/Characters/CharacterItem.tsx
+++ src/components/Characters/CharacterItem.tsx
@@ -9,7 +9,7 @@ export const CharacterItem = ({ character }:Props) => {
   const { name, thumbnail } = character
   const imgURL = `${thumbnail.path}.${thumbnail.extension}`
   return (
-    <Link to={`characters/${name}`} className='character'>
+    <Link to={`/${name}`} className='character'>
       <p>{name}</p>
       <img src={imgURL} alt={name} loading='lazy' />
     </Link>
