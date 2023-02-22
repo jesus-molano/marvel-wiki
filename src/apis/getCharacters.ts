09688fc7308cc3efd5ccc5fac286ae441e3881c3
jesus-molano <jessumolano@gmail.com>
2023-02-22 02:45:01 +0000

    Added simple pagination

--- src/apis/getCharacters.ts
+++ src/apis/getCharacters.ts
@@ -1,6 +1,6 @@
 import { useFetch } from "@hooks/useFetch"
 
-export const getCharacters = (limit: number, offset: number) => {
+export const getCharacters = (limit: number = 10, offset: number = 0) => {
   const url = 'https://akabab.github.io/superhero-api/api/all.json'
   const {data, hasError, isLoading} = useFetch(url)
   return {
