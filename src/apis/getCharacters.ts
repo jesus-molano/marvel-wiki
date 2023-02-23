6b82082eca47aefaa5046be01a4c1a828d98b923
jesus-molano <jessumolano@gmail.com>
2023-02-23 05:41:59 +0000

    Updated .env variables

--- src/apis/getCharacters.ts
+++ src/apis/getCharacters.ts
@@ -2,9 +2,11 @@ import { useFetch } from "@hooks/useFetch"
 
 const API_KEY = import.meta.env.VITE_MARVEL_API_KEY
 const API_URL = import.meta.env.VITE_MARVEL_API_URL
+const API_TS = import.meta.env.VITE_MARVEL_API_TS
+const API_HASH = import.meta.env.VITE_MARVEL_API_HASH
 
 export const getCharacters = (limit: number = 10, offset: number = 0, orderBy: string = 'name') => {
-  const url = `${API_URL}characters?orderBy=${orderBy}&limit=${limit}&offset=${offset}&apikey=${API_KEY}`
+  const url = `${API_URL}characters?orderBy=${orderBy}&limit=${limit}&offset=${offset}&ts=${API_TS}&apikey=${API_KEY}&hash=${API_HASH}`
   const { data, hasError, isLoading } = useFetch(url)
   
   const characters = data?.data?.results
--- src/apis/getSeries.ts
+++ src/apis/getSeries.ts
@@ -1,9 +1,11 @@
 import { useFetch } from "@hooks/useFetch"
 const API_KEY = import.meta.env.VITE_MARVEL_API_KEY
 const API_URL = import.meta.env.VITE_MARVEL_API_URL
+const API_TS = import.meta.env.VITE_MARVEL_API_TS
+const API_HASH = import.meta.env.VITE_MARVEL_API_HASH
 
 export const getSeries = (limit: number = 16, offset: number = 0, orderBy: string = 'title') => {
-  const url = `${API_URL}series?orderBy=${orderBy}&limit=${limit}&offset=${offset}&apikey=${API_KEY}`
+  const url = `${API_URL}series?orderBy=${orderBy}&limit=${limit}&offset=${offset}&ts=${API_TS}&apikey=${API_KEY}&hash=${API_HASH}`
   const { data, hasError, isLoading } = useFetch(url)
   const totalSeries = data?.data?.total
   const series = data?.data?.results
